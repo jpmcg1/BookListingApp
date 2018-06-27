93a45f03c77b80a0c1b367c33791357988d360c5
JP <johnmcgrady@hotmail.co.uk>
2018-06-27 16:35:36 +0900

    Add edit text and button on XML

--- app/src/main/java/com/example/android/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/android/booklistingapp/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.android.booklistingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String mSearchWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.search_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText searchWord = findViewById(R.id.search_word);
+                mSearchWord = searchWord.getText().toString();
+                Log.e("Main Activity", "The search word is......" + mSearchWord);
+            }
+        });
     }
 }
