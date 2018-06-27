713f43c6e1dd677e30ceb1949de8acfbaa7ad636
JP <johnmcgrady@hotmail.co.uk>
2018-06-27 17:46:28 +0900

    Add makeHTTPRequest() method

--- app/src/main/java/com/example/android/booklistingapp/QueryUtils.java
+++ app/src/main/java/com/example/android/booklistingapp/QueryUtils.java
@@ -3,6 +3,8 @@ package com.example.android.booklistingapp;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
@@ -42,5 +44,53 @@ public class QueryUtils {
         return url;
     }
 
+    // Make a HTTP response to the given URL and return a String.
+    // The string is the entire JSON response which is generated using the readFromStream()
+    // method
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
 
+        // If the URL is empty, return early
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        // Create a HttpURLConnection object and InputStream object ready for assignment
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.connect();
+
+            // If the request was successful the response code is 200, read the input stream
+            // and put the response in a String
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e("QueryUtils", "Error response code " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e("QueryUtils", "Problem resolving the JSON results, e");
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) {
+        return null;
+    }
+
+    public static List<Book> extractFeatureFromJson(String bookJsonResponse) {
+        return null;
+    }
 }
